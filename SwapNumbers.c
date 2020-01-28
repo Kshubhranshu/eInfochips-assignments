aaa53a3d119c11d3d9009ae9080c774c9b7cdc4a
Kumar Shubhranshu <shubhranshu14@gmail.com>
2020-01-28 13:45:22 +0530

    validated inputs

--- SwapNumbers.c
+++ SwapNumbers.c
@@ -1,16 +1,50 @@
 /*Program to swap two nos using xor operator*/
-#include<stdio.h>
+#include <stdio.h>
+#include <limits.h>
+
+/*
+* validates the inputs
+*
+* @param iValue
+*
+* @return void
+*/
+void validateInput(int iValue);
 
 void main()
 {
     int iFirstNumber;
     int iSecondNumber;
 
-    printf("Enter first and second integers   : ");
-    scanf("%d %d", &iFirstNumber, &iSecondNumber);
+    int iFlag;  // stores the return value from scanf
 
+    /*inputs*/
+    printf("Enter first number [type: integer][range: %d to %d]: ", INT_MIN, INT_MAX);
+    scanf("%d", &iFirstNumber);
+
+    /*validate input*/
+    validateInput(iFirstNumber);
+    
+    printf("Enter second number [type: integer][range: %d to %d]: ", INT_MIN, INT_MAX);
+    scanf("%d", &iSecondNumber);
+
+    /*validate input*/
+    validateInput(iSecondNumber);
     /*swapping*/
     iFirstNumber = iFirstNumber ^ iSecondNumber;
     iSecondNumber = iFirstNumber ^ iSecondNumber;
     iFirstNumber = iFirstNumber ^ iSecondNumber;
+
+    /*print swapped value*/
+    printf("First integer swapped value : %d\n", iFirstNumber);
+    printf("Second integer swapped value : %d\n", iSecondNumber);
+}
+
+void validateInput(int iValue)
+{
+    if(iValue < INT_MIN || iValue > INT_MAX)
+    {
+        printf("Invalid input\n");
+        printf("Program terminated!!!\n");
+    }
 }
