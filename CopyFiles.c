fabc9b27b772105ab57f47c3008cf18dd9a24964
Kumar Shubhranshu <shubhranshu14@gmail.com>
2020-01-30 18:30:54 +0530

    added file usage and formatted output

--- CopyFiles.c
+++ CopyFiles.c
@@ -2,12 +2,23 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+
+/*
+    * this methods shows the file usage details
+    *
+    * @return void
+*/
+void getFileUsage();
+
 void main()
 {
-    FILE *pSourceFile;  
+    FILE *pSourceFile; 
     FILE *pDestinationFile;
     int iCharacter;     // to copy characters from source file to destination
-    
+
+    /*file usage information*/
+    getFileUsage();
+
     /*stores the address of the file opened*/
     pSourceFile = fopen("file1.txt", "r");
     
@@ -16,6 +27,10 @@ void main()
     {
         printf("Cannot open file\n");
         exit(0);
+    }
+    else
+    {
+        printf("Source file opened successfully \xE2\x9C\x93\n\n");
     }
 
     /*stores the address of the file opened*/
@@ -27,14 +42,32 @@ void main()
         printf("Cannot open file\n");
         exit(0);
     }
+    else
+    {
+        printf("Destination file opened successfully \xE2\x9C\x93 \n\n");
+    }
 
     /*reading data from file using fgetc*/
     while((iCharacter = fgetc(pSourceFile)) != EOF)
     {
         fputc(iCharacter, pDestinationFile);
     }
+    printf("Data copied successfully \xE2\x9C\x93 \n\n");
 
     /*closing the file*/
     fclose(pSourceFile);
+    printf("Source file closed successfully \xE2\x9C\x93 \n\n");
+
     fclose(pDestinationFile);
+    printf("Destination file closed successfully \xE2\x9C\x93 \n\n");
+
+}
+
+void getFileUsage()
+{
+    printf("File Usage\n");
+    printf("Source filename                                       : file1.txt\n");
+    printf("Destination filename[\x1b[31;23mall previous data will be erased\x1b[m]: file2.txt\n");
+    printf("File directory                                        : home/einfochips/Desktop/eInfochips-tasks-master\n");
+    printf("\n");
 }
