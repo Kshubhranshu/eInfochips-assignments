e119b7c2e0f93790179382cf1c6a1552a6e31c49
Kumar Shubhranshu <shubhranshu14@gmail.com>
2020-02-14 18:39:10 +0530

    new assignment related to processes

---
+++ assignment3/Assignment_ProcessandThreads/Task_3_parent_write_ABCD_file_child_write_abcd_file/Task_3_parent_write_ABCD_file_child_write_abcd_file.c
@@ -0,0 +1,8 @@
+
+/******************************************************************************/
+/*! \file   Task_3_parent_write_ABCD_file_child_write_abcd_file.c
+
+  \brief    Make a program where parent writes capital 'ABCD..' into file &
+  child writes small 'abcd ..' into file.
+ *@{*/
+/******************************************************************************/
