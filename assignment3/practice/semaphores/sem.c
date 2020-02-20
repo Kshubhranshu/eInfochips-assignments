9dd3439c9f7c1dff5ffc52bd676ba2f4fa501520
Kumar Shubhranshu <shubhranshu14@gmail.com>
2020-02-20 19:01:23 +0530

    added semaphore process

---
+++ assignment3/practice/semaphores/sem.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <semaphore.h>
+
+sem_t sem;
+
+int num = 0;
+
+void *fun1(void *);
+void *fun2(void *);
+
+int main(void)
+{
+	pthread_t thread1;	
+	pthread_t thread2;
+	
+	sem_init(&sem, 0, 1);
+
+	pthread_create(&thread1, NULL, fun1, (void *)&num);
+	pthread_create(&thread2, NULL, fun2, (void *)&num);
+	pthread_join(thread1, NULL);
+	pthread_join(thread2, NULL);
+
+	sem_destroy(&sem);
+}
+
+void *fun1(void * arg)
+{
+	sem_wait(&sem);
+	printf("num incrented by thread 1 : %d\n", ++(num));
+	sleep(3);
+	sem_post(&sem);
+	pthread_exit(NULL);
+}
+
+void *fun2(void * arg)
+{
+	sem_wait(&sem);
+	printf("num incremented by thread 2 : %d\n", ++(num));
+	sleep(3);
+	sem_post(&sem);
+	pthread_exit(NULL);
+}
