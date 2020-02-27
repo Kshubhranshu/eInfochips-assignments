a60e0a8048d59cc8f8fd9b3809513ff50ebe5d08
Kumar Shubhranshu <shubhranshu14@gmail.com>
2020-02-27 19:03:35 +0530

    system v semaphores

--- assignment3/practice/sysVsemaphore/sem1.c
+++ assignment3/practice/sysVsemaphore/sem1.c
@@ -1,25 +1,200 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-
 #include <sys/sem.h>
+#include <stdio.h>
+#include <semaphore.h>
+#include <stdlib.h>
+#include <pthread.h>
 
-#include "semun.h"
+#define THREAD_SIZ 2
 
-static int set_semvalue(void);
-static void del_semvalue(void);
-static int semaphore_p(void);
-static int semaphore_v(void);
+int count = 0;
+sem_t sem_id;
 
-static int sem_id;
+void *threadFunction1(void *arg);
+
+void *threadFunction2(void *arg);
+
+int initsem(key_t key, int nsems);
+
+int sem_create(key_t key);
+
+int sem_init(sem_t *semid, int pshared, unsigned int value);
+
+int sem_initialize(sem_t *semid, int value);
+
+int sem_destroy(sem_t *semid);
+
+int sem_wait(sem_t *semid);
+
+int sem_trywait(sem_t *semid);
+
+int sem_post(sem_t *semid);
 
 int main(void)
 {
-	int i;
-	int pause_time;
-	char op_char = '0';
+	pthread_t thread[THREAD_SIZ];
 
-	srand((unsigned int)getpid());
+	sem_init(&sem_id, 0, 1);
 
-	sem_id = semget((key_t)1234, 1, 0);
+	pthread_create(&thread[0], NULL, &threadFunction1, NULL);
+	pthread_create(&thread[1], NULL, &threadFunction2, NULL);
+	
+	pthread_join(thread[0], NULL);
+	pthread_join(thread[1], NULL);
+
+	sem_destroy(&sem_id);
+}
+
+void *threadFunction1(void *arg)
+{
+	int ret;
+
+	ret = sem_trywait(&sem_id);
+	if(ret == 0)
+	{
+		printf("Thread 1 locked...\n");
+		count++;
+		printf("count value(thread 1): %d\n", count);
+		sem_post(&sem_id);
+		printf("Thread 1 lock realeased...\n");
+	}
+	return NULL;
+}
+
+void *threadFunction2(void *arg)
+{
+	int ret;
+
+	ret = sem_wait(&sem_id);
+	if(ret == 0)
+	{
+		printf("Thread 2 locked...\n");
+		count++;
+		printf("count value(thread 1): %d\n", count);
+		sem_post(&sem_id);
+		printf("Thread 1 lock realeased...\n");
+	}
+	return NULL;
+}
+
+int initsem(key_t key, int nsems)
+{
+	int semid;
+
+	semid = semget(key, nsems, IPC_CREAT | IPC_EXCL | 0666);
+	if(semid == -1)
+	{
+		perror("Semaphore identifier failed!!!\n");
+		return -1;
+		exit(EXIT_FAILURE);
+	}
+	return semid;
+}
+
+int sem_initialize(sem_t *semid, int value)
+{
+	int ret;
+
+	ret = semctl(*((int *)semid), 0, SETVAL, value);
+	if(ret == -1)
+	{
+		perror("Failed to initialize control!!!\n");
+		return ret;
+		exit(EXIT_FAILURE);
+	}
+	return ret;
+}
+
+int sem_init(sem_t *sem, int pshared, unsigned int value)
+{
+	int ret = -1;
+	key_t key = 5600;
+	
+	if(value > 1)
+	{
+		perror("Semaphore value limit exceeded!\n");
+		return ret;
+		exit(EXIT_FAILURE);
+	}
+
+	if(((*((int *)sem)) = initsem(key,1)) == -1)
+	{
+       return -1;
+	   exit(EXIT_FAILURE);
+  	}
+
+	ret = sem_initialize(sem, value);
+	if(ret == -1)
+	{
+		perror("Semaphore initialization failed\n");
+		return ret;
+		exit(EXIT_FAILURE);
+	}
+	return ret;
+}
+
+int sem_destroy(sem_t *semid)
+{
+	int ret;
+
+	ret = semctl(*((int *)semid), 0, IPC_RMID);
+	if(ret == -1)
+	{
+		perror("Semphore failed to destroy!\n");
+		exit(EXIT_FAILURE);
+	}
+	return ret;
+}
+
+int sem_wait(sem_t *semid)
+{
+	int ret;
+	struct sembuf sb;
+	
+	sb.sem_num = 0;
+	sb.sem_op = -1;
+	sb.sem_flg = 0;
+
+	ret = semop(*((int *)semid), &sb, 1);
+	if(ret == -1)
+	{
+		perror("Semaphore try wait failed!n\n");
+		exit(EXIT_FAILURE);
+	}
+	return ret;
+}
+
+int sem_trywait(sem_t *semid)
+{
+	int ret;
+	struct sembuf sb;
+	
+	sb.sem_num = 0;
+	sb.sem_op = -1;
+	sb.sem_flg = IPC_NOWAIT;
+
+	ret = semop(*((int *)semid), &sb, 1);
+	if(ret == -1)
+	{
+		perror("Semaphore try wait failed!n\n");
+		exit(EXIT_FAILURE);
+	}
+	return ret;
+}
+
+int sem_post(sem_t *semid)
+{
+	int ret;
+	struct sembuf sb;
+	
+	sb.sem_num = 0;
+	sb.sem_op = 1;
+	sb.sem_flg = 0;
+
+	ret = semop(*((int*)semid), &sb, 1);
+	if(ret == -1)
+	{
+		perror("Semaphore try wait failed!n\n");
+		exit(EXIT_FAILURE);
+	}
+	return ret;
 }
