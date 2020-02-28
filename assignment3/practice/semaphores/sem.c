dcbeca72d1f312e85990b23e20735deb171a0a34
Kumar Shubhranshu <shubhranshu14@gmail.com>
2020-02-28 18:21:57 +0530

    checked for race condition

--- assignment3/practice/semaphores/sem.c
+++ assignment3/practice/semaphores/sem.c
@@ -16,8 +16,7 @@ int main(void)
 	pthread_t thread1;	
 	pthread_t thread2;
 	
-	sem_init(&sem, 0, 0);
-	//sem_init(&sem, 0, 1);
+	sem_init(&sem, 0, 1);
 
 	pthread_create(&thread1, NULL, fun1, (void *)&num);
 	pthread_create(&thread2, NULL, fun2, (void *)&num);
@@ -29,18 +28,22 @@ int main(void)
 
 void *fun1(void * arg)
 {
-	//sem_init(&sem, 0, 0);
+	//sem_wait(&sem);
+	for(int i = 0; i < 1000; i++)
+	{
 	printf("num incrented by thread 1 : %d\n", ++(num));
-	sleep(3);
-	sem_post(&sem);
+	}
+	//sem_post(&sem);
 	pthread_exit(NULL);
 }
 
 void *fun2(void * arg)
 {
-	sem_wait(&sem);
-	printf("num incremented by thread 2 : %d\n", ++(num));
-	sleep(3);
-	sem_post(&sem);
+	//sem_wait(&sem);
+	for(int i = 0; i < 1000; i++)
+	{
+	printf("num decremented by thread 2 : %d\n", --(num));
+	}
+	//sem_post(&sem);
 	pthread_exit(NULL);
 }
