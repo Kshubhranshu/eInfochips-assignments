c3a36e747aff870a54722ad885c7da579980934e
Kumar Shubhranshu <shubhranshu14@gmail.com>
2020-03-02 19:11:43 +0530

    added sharedmem progs

--- assignment3/practice/sharedmem/shm2.c
+++ assignment3/practice/sharedmem/shm2.c
@@ -1,15 +1,15 @@
-#include <unistd.h>
-#include <stdlib.h>
+#include "shm_com.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include <string.h>
 #include <sys/shm.h>
-#include "shm_com.h"
 
-int main(void)
+int main()
 {
 	char *str;
 	int shmid;
-	
+
 	shmid = shmget((key_t)1234, 1024, 0666 | IPC_CREAT);
 
 	if(shmid == -1)
@@ -17,17 +17,23 @@ int main(void)
 		perror("shmget failed\n");
 		exit(EXIT_FAILURE);
 	}
-
 	str = (char *)shmat(shmid, NULL, 0);
-	if(str == (void*)-1)
+	if(shared_memory == (void *)-1)
 	{
 		perror("shmat failed\n");
 		exit(EXIT_FAILURE);
 	}
 
-	printf("Memory attached at %lu\n", (unsigned long)str);
+	printf("memory attached at %lu\n", (unsigned long)str);
 
-	scanf("%s", str);
-	
-	shmdt(str);
+	printf("length of : %s\n", str);
+		
+
+	if(shmdt(str) == -1)
+	{
+		perror("shmdt failed\n");
+		exit(EXIT_FAILURE);
+	}
+
+ 	shmctl(shmid, IPC_RMID, NULL);	
 }
--- assignment3/practice/sharedmem/shm_com.h
+++ assignment3/practice/sharedmem/shm_com.h
@@ -1,5 +1,5 @@
 #define TEXT_SIZ 2048
-
+void * shared_memory;
 struct shared_use_st
 {
 	int written_by_you;
